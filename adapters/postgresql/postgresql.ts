ae874e164f5a8127b6bfc2afeae7c5d4f0cb9be7
Bhunmas <lovebhunma@outlook.com>
2025-05-01 11:33:14 +0700

    product  catagory api hiden when inactive

--- adapters/postgresql/postgresql.ts
+++ adapters/postgresql/postgresql.ts
@@ -71,7 +71,7 @@ export class InPostgresqlProductRepository implements IOrderDbResponsitory {
     readOneCatagory(catagory: string): Promise<OrderDb> {
         return new Promise(async (resolve, reject) => {
             const connect = await this.client.connect();
-            const res = await this.client.query(`SELECT * FROM products WHERE category = '${catagory}' ORDER BY  product_id ASC`);
+            const res = await this.client.query(`SELECT * FROM products WHERE active = true and category = '${catagory}' ORDER BY  product_id ASC`);
             console.log("Res :", catagory)
             if (res.rows.length <= 0) {
                 reject(res);
